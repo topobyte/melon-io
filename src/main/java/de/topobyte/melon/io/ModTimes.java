46286950835f014bee9da191f9077b2d4bf38ab9
Sebastian Kürten <sebastian@topobyte.de>
2021-02-20 06:34:34 +0100

    Add ModTimes

---
+++ src/main/java/de/topobyte/melon/io/ModTimes.java
@@ -0,0 +1,51 @@
+// Copyright 2021 Sebastian Kuerten
+//
+// This file is part of melon-io.
+//
+// melon-io is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// melon-io is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with melon-io. If not, see <http://www.gnu.org/licenses/>.
+
+package de.topobyte.melon.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileTime;
+
+public class ModTimes
+{
+
+	/**
+	 * Test if 'file' has been modified after all of 'others'. Typically used in
+	 * build systems where an artifact depends on a number of other files and
+	 * needs to be recreated only if any of the other files has been modified
+	 * after the artifact itself. This method can be used to check if an
+	 * artifact is up to date.
+	 */
+	public static boolean isNewerThan(Path file, Path... others)
+			throws IOException
+	{
+		if (!Files.exists(file)) {
+			return false;
+		}
+		FileTime mod = Files.getLastModifiedTime(file);
+		for (Path other : others) {
+			FileTime otherMod = Files.getLastModifiedTime(other);
+			if (otherMod.compareTo(mod) > 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
---
+++ src/test/java/de/topobyte/melon/io/TestModTimes.java
@@ -0,0 +1,110 @@
+// Copyright 2021 Sebastian Kuerten
+//
+// This file is part of melon-io.
+//
+// melon-io is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// melon-io is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with melon-io. If not, see <http://www.gnu.org/licenses/>.
+
+package de.topobyte.melon.io;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileTime;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestModTimes
+{
+
+	private Path file1;
+	private Path file2;
+	private Path file3;
+	private Path file4;
+	private Path file5;
+
+	@Before
+	public void setup() throws IOException
+	{
+		// file1 = file2 < file3 < file4
+		file1 = Files.createTempFile("file", null);
+		file2 = Files.createTempFile("file", null);
+		file3 = Files.createTempFile("file", null);
+		file4 = Files.createTempFile("file", null);
+		file5 = Files.createTempFile("file", null);
+		delete(file5);
+		long base = 1460000000;
+		Files.setLastModifiedTime(file1, FileTime.from(base, TimeUnit.SECONDS));
+		Files.setLastModifiedTime(file2, FileTime.from(base, TimeUnit.SECONDS));
+		Files.setLastModifiedTime(file3,
+				FileTime.from(base + 60, TimeUnit.SECONDS));
+		Files.setLastModifiedTime(file4,
+				FileTime.from(base + 120, TimeUnit.SECONDS));
+	}
+
+	@After
+	public void cleanUp()
+	{
+		delete(file1);
+		delete(file2);
+		delete(file3);
+		delete(file4);
+	}
+
+	private void delete(Path path)
+	{
+		try {
+			Files.deleteIfExists(path);
+		} catch (IOException e) {
+			// ignore
+		}
+	}
+
+	@Test
+	public void test() throws IOException
+	{
+		// comparison is not strict!
+		assertTrue(ModTimes.isNewerThan(file2, file1));
+		assertTrue(ModTimes.isNewerThan(file1, file2));
+
+		assertFalse(ModTimes.isNewerThan(file2, file3));
+		assertFalse(ModTimes.isNewerThan(file3, file4));
+		assertFalse(ModTimes.isNewerThan(file2, file4));
+
+		assertTrue(ModTimes.isNewerThan(file3, file2));
+		assertTrue(ModTimes.isNewerThan(file4, file3));
+		assertTrue(ModTimes.isNewerThan(file4, file2));
+
+		assertTrue(ModTimes.isNewerThan(file4, file2, file3));
+
+		assertFalse(ModTimes.isNewerThan(file2, file3, file4));
+		assertFalse(ModTimes.isNewerThan(file3, file2, file4));
+
+		assertFalse(ModTimes.isNewerThan(file5, file2));
+		assertFalse(ModTimes.isNewerThan(file5, file2, file3));
+	}
+
+	@Test(expected = NoSuchFileException.class)
+	public void testException() throws IOException
+	{
+		ModTimes.isNewerThan(file2, file5);
+	}
+
+}
